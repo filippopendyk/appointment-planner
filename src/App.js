6e2ba6e92821a3805459a75f3d48dbb319aa0b01
filippopendyk <filipos28511@gmail.com>
2022-10-24 14:59:24 -0400

    adds contacts and appointment handling to app

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
+import { useState } from "react";
+import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
+import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
+function App() {
+  /*
+  Define state variables for 
+  contacts and appointments 
+  */
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
+
+  const ROUTES = {
+    CONTACTS: "/contacts",
+    APPOINTMENTS: "/appointments",
+  };
+
+  /*
+  Implement functions to add data to
+  contacts and appointments
+  */
+
+  const addAppointment = (appointment) => {
+    setAppointments(prev => [...prev, appointment]);
+  }
+
+  const addContact = (contact) => {
+    setContacts(prev => [...prev, contact]);
+  }
+
+  return (
+    <>
+      <nav>
+        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
+          Contacts
+        </NavLink>
+        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
+          Appointments
+        </NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to={ROUTES.CONTACTS} />
+          </Route>
+          <Route path={ROUTES.CONTACTS}>
+             {/* Add props to ContactsPage */}
+            <ContactsPage />
+          </Route>
+          <Route path={ROUTES.APPOINTMENTS}>
+            {/* Add props to AppointmentsPage */}
+            <AppointmentsPage />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default App;
