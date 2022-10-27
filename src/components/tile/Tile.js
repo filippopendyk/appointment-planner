8446bfcae2269cf296423ff1ee02770f8205b659
filippopendyk <filipos28511@gmail.com>
2022-10-27 00:41:49 -0400

    refactors the tile component with object.values

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({item}) => {
+  const itemAsArray = Object.values(item);
+  
   return (
     <div className="tile-container">
-      
+      {itemAsArray.map((item,index) => {
+        if(index === 0){
+          return <p className="tile-title">{item}</p>
+        }
+        return <p className="tile">{item}</p>
+      })}
     </div>
   );
 };
