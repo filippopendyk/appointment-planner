741d3f5d676ac5716223e0738bf3c811244ab8b5
filippopendyk <filipos28511@gmail.com>
2022-10-27 01:00:31 -0400

    makes the submit function for appointment

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({appointments, contacts, addApointment}) => {
   /*
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+    addApointment(title, contact, date, time);
+    setTitle("");
+    setContact("");
+    setDate("");
+    setTime("");
   };
 
   return (
